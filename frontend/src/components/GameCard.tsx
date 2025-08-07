2d4fe8db597ceb820f195836a39b6e5862151db6
Kudaiberdi <kdajberdiz@gmail.com>
2025-08-07 21:40:33 +0500

    Adding emodji to card

--- frontend/src/components/GameCard.tsx
+++ frontend/src/components/GameCard.tsx
@@ -2,6 +2,9 @@ import type { Game } from "../hooks/useGames";
 import getCroppedImageUrl from "../services/img-url";
 import CriticScore from "./CriticScore";
 import PlatformIconList from "./PlatformIconList";
+import Norm from "../assets/meh.webp";
+import Good from "../assets/thumbs-up.webp";
+import Meh from "../assets/bulls-eye.webp";
 
 interface Props {
   game: Game;
@@ -12,6 +15,18 @@ const GameCard = ({ game }: Props) => {
     game.metacritic > 75
       ? "bg-success border-success"
       : "bg-warning border-warning";
+
+  let emj = { src: "", size: "", alt: "" };
+
+  if (game.metacritic) {
+    if (game.metacritic > 89)
+      emj = { src: Meh, size: "35px", alt: "exceptional" };
+    else if (game.metacritic > 75)
+      emj = { src: Good, size: "25px", alt: "recommended" };
+    else if (game.metacritic > 60)
+      emj = { src: Norm, size: "25px", alt: "meh" };
+  }
+
   return (
     <>
       <div className="card border rounded">
@@ -21,7 +36,6 @@ const GameCard = ({ game }: Props) => {
           alt="game photo"
         />
         <div className="card-body">
-          <h4 className="text-center"> {game.name} </h4>
           <div className="d-flex justify-content-between">
             <div>
               {game.parent_platforms && (
@@ -36,6 +50,8 @@ const GameCard = ({ game }: Props) => {
               </div>
             )}
           </div>
+          <h4 className="p-1"> {game.name} </h4>
+          <img src={emj.src} height={emj.size} alt={emj.alt} />
         </div>
       </div>
     </>
