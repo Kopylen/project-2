cffc920ca1a220d2759bb96039083908c56fc975
Kudaiberdi <kdajberdiz@gmail.com>
2025-08-16 19:49:20 +0500

    .

--- frontend/src/components/GameHeading.tsx
+++ frontend/src/components/GameHeading.tsx
@@ -5,7 +5,7 @@ interface Props {
 }
 
 const GameHeading = ({ gameQuery }: Props) => {
-  const heading = `${gameQuery?.platform?.name || "All"} ${
+  const heading = `${gameQuery?.platform?.name || "Roma 4ert"} ${
     gameQuery?.genre?.name || ""
   } Games`;
   return <h1 className="display-2 pb-2 fw-bold">{heading}</h1>;
